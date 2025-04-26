80c5eb495bdac377905ea0fcc086ba1c2f2dcd96
Arbaz khan <arbazkhan@Arbazs-MacBook-Air.local>
2025-04-26 17:54:16 +0530

    deployed backend force Hitting--

--- lib/hono.ts
+++ lib/hono.ts
@@ -2,7 +2,7 @@ import { hc } from "hono/client";
 
 import { AppType } from "@/app/api/[[...route]]/route";
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL || "https://finanace-antonio-7e6a.vercel.app/");
 
 
 
